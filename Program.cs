636c35e9132a71cf4d5771a65e87e0df9c387620
TorobaevSher <108340083+TorobaevSher@users.noreply.github.com>
2022-07-01 06:36:18 +0600

    Lesson 1.8 задача

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, 
+//которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа 
+//от 1 до N.
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int index=2; index<=num; index++)
+if(index%2==0)
+Console.WriteLine($"Четные числа{index} ");
